2dd4c62f87890b261039e6d0a69442fa8eb251e5
bhanuprasadreddy657-rgb <bhanuprasadreddy657@gmail.com>
2026-07-28 15:32:15 +0530

    Add comments to ddl_gold.sql for clarity
    Added documentation for the gold views creation script.

--- SCRIPTS/gold/ddl_gold.sql
+++ SCRIPTS/gold/ddl_gold.sql
@@ -1,1 +1,16 @@
+/**
+================================================================================
+DDL script:create gold views 
+================================================================================
+script purpose:
+         this script creates the views for gold year in data ware house.
+         the gold layer represents the dimention and fact tables (star schema)
 
+         each view performs transformations combines data from silver layer 
+         to producea clean and structured,enriched and business ready dataset.
+USAGE:
+      this views can be queried directly for analytics and reporting.
+/**
+================================================================================
+CREATE DIMENSION:gold.dim_customers 
+================================================================================
