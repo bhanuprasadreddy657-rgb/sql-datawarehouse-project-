feffe3e8378a6d0206333613c6f7dfea707d7e77
bhanuprasadreddy657-rgb <bhanuprasadreddy657@gmail.com>
2026-07-28 15:34:59 +0530

    Add views for gold.dim_products and gold.fact_sales
    Added SQL commands to create dimension and fact views for gold schema.

--- SCRIPTS/gold/ddl_gold.sql
+++ SCRIPTS/gold/ddl_gold.sql
@@ -41,6 +41,9 @@ ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON ci.cst_key = la.cid
 
+===========================================================================
+CREATE DIMENSION:gold.dim_products
+===========================================================================
 
 IF OBJECT_ID('gold.dim_products','v') IS NOT NULL 
    DROP VIEW gold.dim_products;
@@ -65,6 +68,9 @@ LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL -- FILTER OUT HISTORICAL DATA
 
+=======================================================================
+CREATE FACT: gold.fact_sales
+=======================================================================
 
 IF OBJECT_ID('gold.fact_sales','V') IS NOT NULL 
    DROP VIEW gold.fact_sales 
